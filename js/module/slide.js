84990983653e7c601e065e81fa81a107bc6e9958
Pedro <you@example.com>
2020-10-16 13:56:16 -0300

    adicionando navegação de slide(prev e next)

--- js/module/slide.js
+++ js/module/slide.js
@@ -6,7 +6,7 @@ export default class Slide {
     this.dist = { finalPosition: 0, startX: 0, moviment: 0 };
   }
 
-  // pega a primeira posição do mouse e chama a event de mover o mouse(mousemove);
+  // 4º pega a primeira posição do mouse e chama a event de mover o mouse(mousemove);
   onStart(event) {
     let moveEvent;
 
@@ -20,20 +20,22 @@ export default class Slide {
     }
 
     this.wrapper.addEventListener(moveEvent, this.onMouseMove);
+    this.transition(false);
   }
 
-  // responsável por mover o slide pelo style;
+  // 6º responsável por mover o slide pelo style;
   moveSlide(distX) {
     this.dist.movePosition = distX; // posição final;
     this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
   }
 
-  // atualiza a posição do slide;
+  // 6º atualiza a posição do slide;
   updatePosition(clientX) {
     this.dist.moviment = (this.dist.startX - clientX) * 1.6;
     return this.dist.finalPosition - this.dist.moviment;
   }
 
+  // 5º event de mousemove. reponsavel por chamar as demais.
   onMouseMove(event) {
     const pointerPosition =
       event.type === "mousemove"
@@ -50,32 +52,32 @@ export default class Slide {
 
     this.wrapper.removeEventListener(moveEvent, this.onMouseMove);
     this.dist.finalPosition = this.dist.movePosition;
+    this.transition(true);
+    this.changeSlideOnEnd();
   }
 
-  // adiciona os events.
-  addSlideEventListener() {
-    // o this desse está errado, para consertar usamos o bind
-    this.wrapper.addEventListener("mousedown", this.onStart);
-    this.wrapper.addEventListener("touchstart", this.onStart);
-    this.wrapper.addEventListener("mouseup", this.onEnd);
-    this.wrapper.addEventListener("touchend", this.onEnd);
+  transition(active) {
+    this.slide.style.transition = active ? "transform .3s ease" : "";
   }
 
-  // altera o this dos métodos
-  onBind() {
-    this.onStart = this.onStart.bind(this);
-    this.onEnd = this.onEnd.bind(this);
-    this.onMouseMove = this.onMouseMove.bind(this);
+  changeSlideOnEnd() {
+    if (this.dist.moviment > 120 && this.index.next !== undefined) {
+      this.activeNextSlide();
+    } else if (this.dist.moviment < -120 && this.index.prev !== undefined) {
+      this.activePrevSlide();
+    } else {
+      this.changeSlide(this.index.active);
+    }
   }
 
-  // vai posicionar ao centro
+  // 2º³ vai posicionar ao centro
   slidePosition(slide) {
     const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
 
     return -(slide.offsetLeft - margin);
   }
 
-  // vai configurar o slide, passando ele uma array conseguimos editar eles.
+  // 2º² vai configurar o slide, passando ele uma array conseguimos editar eles.
   slideConfig() {
     this.slideArray = [...this.slide.children].map((item) => {
       const position = this.slidePosition(item);
@@ -83,6 +85,7 @@ export default class Slide {
     });
   }
 
+  // 7º pega o index do slide, para isso funcionar temos que transformar ele(itens do slide) um array
   slideIndexNav(index) {
     const last = this.slideArray.length - 1;
 
@@ -93,12 +96,37 @@ export default class Slide {
     };
   }
 
+  // 7º muda o slide de acordo com seu index;
   changeSlide(index) {
     const activeSlide = this.slideArray[index];
-    this.moveSlide(activeSlide);
+    this.moveSlide(activeSlide.position);
     this.slideIndexNav(index);
 
-    this.dist.finalPosition = activeSlide;
+    this.dist.finalPosition = activeSlide.position;
+  }
+
+  // muda o slide para o anterior
+  activePrevSlide() {
+    if (this.index.prev !== undefined) this.changeSlide(this.index.prev);
+  }
+  activeNextSlide() {
+    if (this.index.active !== undefined) this.changeSlide(this.index.next);
+  }
+
+  // 2º adiciona os events.
+  addSlideEventListener() {
+    // o this desse está errado, para consertar usamos o bind
+    this.wrapper.addEventListener("mousedown", this.onStart);
+    this.wrapper.addEventListener("touchstart", this.onStart);
+    this.wrapper.addEventListener("mouseup", this.onEnd);
+    this.wrapper.addEventListener("touchend", this.onEnd);
+  }
+
+  // 3º altera o this dos métodos
+  onBind() {
+    this.onStart = this.onStart.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
   }
 
   init() {
